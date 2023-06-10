6161ef680dda75ab28ca010e6d4612fca359d44d
Riel <jasper.apos@gmail.com>
2023-06-10 22:18:05 +0800

    Changed printf to show how many times number iterated

--- Programming Course/2nd sem/printnegative1.c
+++ Programming Course/2nd sem/printnegative1.c
@@ -4,11 +4,13 @@ int main() {
     int store[100];
     int *pointer;
     pointer = store;
+    int i = 1;
 
     while (pointer < &store[100]) {
         *pointer = -1;
-        printf("%d\n", *pointer);
+        printf("%d: %d\n", i, *pointer);
         pointer++;
+        i++;
     }
 
 }
